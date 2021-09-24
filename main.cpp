d16e9bb04337f589cf4a3c66e681ee664292801f
Snir Shekel <88346802+Snir-Shekel@users.noreply.github.com>
2021-09-25 02:11:04 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,23 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+int main()
+{
+
+
+    if(sf::Joystick::isConnected(0))
+        std::cout <<"Joystick 1 is Connected" ;
+    else
+        std::cout <<"Joystick 1 is not Connected" ;
+
+
+
+
+    if(sf::Joystick::isButtonPressed(0, 1))
+        std::cout <<"button" ;
+    else if(sf::Joystick::isButtonPressed(0, 2))
+        std::cout <<"button 2" ;
+
+    return 0;
+}
 
